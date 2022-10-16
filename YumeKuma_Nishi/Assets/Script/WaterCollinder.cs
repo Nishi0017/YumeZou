7f395d1d4555baa75c84e1a24d33a5b8790d8a38
Nishi0017 <c2216@st.cs.kumamoto-u.ac.jp>
2022-10-16 16:13:08 +0900

    a

---
+++ YumeKuma_Nishi/Assets/Script/WaterCollinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCollinder : MonoBehaviour
+{
+    //private ZibraLiquidCollider col = null;
+    void Start()
+    {
+        //col = GetComponent<ZibraLiquidCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ぶつかった！");
+    }
+}
+
