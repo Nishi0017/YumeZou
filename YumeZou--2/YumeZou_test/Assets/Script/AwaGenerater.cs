e87d5525e622591d7944965ebb30e52174e704df
shunsukemori0417 <c2252@st.cs.kumamoto-u.ac.jp>
2022-10-18 20:09:41 +0900

    add

---
+++ YumeZou--2/YumeZou_test/Assets/Script/AwaGenerater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwaGenerater: MonoBehaviour
+{
+    // プレハブ格納用
+    public GameObject CubePrefab;
+    private Vector3 clonePos;
+
+    void Start()
+    {
+        clonePos = this.transform.position;
+    }
+
+    void Update()
+    {
+        // 一定時間ごとにプレハブを生成
+        if (Time.frameCount % 20 == 0)
+        {
+            // 生成位置
+            Vector3 pos = clonePos;
+            // プレハブを指定位置に生成
+            Instantiate(CubePrefab, pos, Quaternion.identity);
+        }
+    }
+}
