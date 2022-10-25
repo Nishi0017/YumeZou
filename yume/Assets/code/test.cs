bb9a0cabf3ef0894ec2ec303a027bec2e6b93ce5
永渕智章 <c2211@st.cs.kumamoto-u.ac.jp>
2022-10-25 20:49:06 +0900

    a

---
+++ yume/Assets/code/test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Transform targetObject; // 注視したいオブジェクトをInspectorから入れておく
+    private Vector3 input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 補完スピードを決める
+        float speed = 0.01f;
+        // ターゲット方向のベクトルを取得
+        Vector3 relativePos = new Vector3(targetObject.position.x, this.transform.position.y, targetObject.position.z) - this.transform.position;
+        // 方向を、回転情報に変換
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        // 現在の回転情報と、ターゲット方向の回転情報を補完する
+        transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, speed);
+    }
+}
